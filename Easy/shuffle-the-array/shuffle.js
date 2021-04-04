db2ed54cf0ff3b7889b7999e39e83a030b84615e
Shane Upchurch <shaneupchurch@gmail.com>
2021-04-04 15:58:33 -0500

    problem with psuedo code and tests setup

---
+++ Easy/shuffle-the-array/shuffle.js
@@ -0,0 +1,15 @@
+
+//link: https://leetcode.com/problems/shuffle-the-array/
+
+function shuffle(nums, n) {
+  //initialize a new array
+  //push the first value of the original array into the new array
+  //push the value at n into the new array next
+  //alternate this process back and forth, iterating by 1 each time until the array length has been reached
+  //return the new array
+
+}
+
+module.exports = {
+    shuffle
+};
---
+++ Easy/shuffle-the-array/shuffle.test.js
@@ -0,0 +1,20 @@
+const { shuffle } = require('./shuffle');
+
+describe('shuffle fn', () => {
+    it('takes in an array in the form [x1,x2,...,xn,y1,y2,...,yn] and a number, and returns an array in the form [x1,y1,x2,y2,...,xn,yn]', () => {
+        const expected1 = [2, 3, 5, 4, 1, 7];
+        const actual1 = shuffle([2, 5, 1, 3, 4, 7], 3);
+        
+        expect(actual1).toEqual(expected1);
+
+        const expected2 = [1, 4, 2, 3, 3, 2, 4, 1];
+        const actual2 = shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4);
+
+        expect(actual2).toEqual(expected2);
+
+        const expected3 = [1, 2, 1, 2];
+        const actual3 = shuffle([1, 1, 2, 2], 2);
+
+        expect(actual3).toEqual(expected3);
+    });
+});
