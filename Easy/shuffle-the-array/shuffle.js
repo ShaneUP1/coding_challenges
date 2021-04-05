c55d022679df6303264e78adb00840756e9aa4ca
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-05 09:47:25 -0400

    messy but close. need for loop

--- Easy/shuffle-the-array/shuffle.js
+++ Easy/shuffle-the-array/shuffle.js
@@ -3,13 +3,24 @@
 
 function shuffle(nums, n) {
   //initialize a new array
+  let arr = [];
   //push the first value of the original array into the new array
+  arr.push(nums[0]);
   //push the value at n into the new array next
+  arr.push(n);
   //alternate this process back and forth, iterating by 1 each time until the array length has been reached
-  //return the new array
+  arr.push(nums[0 + 1]);
+  arr.push(nums[n + 1]);
+  // console.log(nums[n + 1]);
 
+  arr.push(nums[0 + 2]);
+  arr.push(nums[nums.length - 1]);
+
+  console.log('array:', arr);
+  //return the new array
+  return arr;
 }
 
 module.exports = {
-    shuffle
+  shuffle
 };
