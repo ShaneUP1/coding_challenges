c73e1b6a9d51d3278e9b472a336bba54a0b38dd7
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-05 09:52:39 -0400

    passing with for loop

--- Easy/shuffle-the-array/shuffle.js
+++ Easy/shuffle-the-array/shuffle.js
@@ -5,18 +5,17 @@ function shuffle(nums, n) {
   //initialize a new array
   let arr = [];
   //push the first value of the original array into the new array
-  arr.push(nums[0]);
   //push the value at n into the new array next
-  arr.push(n);
   //alternate this process back and forth, iterating by 1 each time until the array length has been reached
-  arr.push(nums[0 + 1]);
-  arr.push(nums[n + 1]);
-  // console.log(nums[n + 1]);
+  for (let i = 0; i < n; i++) {
+    arr.push(nums[i]);
+    // nums[0],
+    // nums[1]...
+    arr.push(nums[n + i]);
+    // nums[3 + 0],
+    // nums[3 + 1]...
+  }
 
-  arr.push(nums[0 + 2]);
-  arr.push(nums[nums.length - 1]);
-
-  console.log('array:', arr);
   //return the new array
   return arr;
 }
