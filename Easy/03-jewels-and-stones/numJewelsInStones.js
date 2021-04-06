8857889c8b948a5cbc1be35e8f77ce584c61fdde
Shane Upchurch <shaneupchurch@gmail.com>
2021-04-05 19:15:58 -0500

    adds problem 3 and test

---
+++ Easy/03-jewels-and-stones/numJewelsInStones.js
@@ -0,0 +1,9 @@
+// link: https://leetcode.com/problems/jewels-and-stones/
+
+function numJewelsInStones(jewels, stones) {
+
+}
+
+module.exports = {
+    numJewelsInStones
+};
---
+++ Easy/03-jewels-and-stones/numJewelsInStones.test.js
@@ -0,0 +1,13 @@
+const { numJewelsInStones } = require('./numJewelsInStones');
+
+describe('numJewelsInStones fn', () => {
+    it('takes in 2 strings, jewels and stones respectively, and return an integer describing how many total jewels are found in the stones', () => {
+        const expected1 = 3;
+        const actual1 = numJewelsInStones('aA', 'aAAbbbb');
+
+        expect(expected1).toEqual(actual1);
+
+        const expected2 = 0;
+        const actual2 = numJewelsInStones('z', 'ZZ');
+    });
+});
