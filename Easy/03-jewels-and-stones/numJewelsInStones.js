eddd99ddfe1b74248217651958c3a04da05ff00f
Shane Upchurch <shaneupchurch@gmail.com>
2021-04-05 19:21:46 -0500

    adds problem 3-easy with psuedo code and test

--- Easy/03-jewels-and-stones/numJewelsInStones.js
+++ Easy/03-jewels-and-stones/numJewelsInStones.js
@@ -1,7 +1,11 @@
 // link: https://leetcode.com/problems/jewels-and-stones/
 
 function numJewelsInStones(jewels, stones) {
-
+  //initialize an empty "answer" array
+  //compare the first letter in jewels to each letter in stones and add 1 to the answer array for each match
+  //**constraints say that jewels will always be <= 1 and that all characters of jewels are unique**
+  //repeat process until you've checked each letter in jewels and return the answer array
+  //bonus points for regex!
 }
 
 module.exports = {
