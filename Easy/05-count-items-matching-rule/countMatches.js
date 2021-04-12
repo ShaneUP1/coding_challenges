25e93ce692e8f8141de2d2348b5d471a2353a31b
Shane Upchurch <shaneupchurch@gmail.com>
2021-04-11 20:38:00 -0500

    adds problem 5 of easy and test

---
+++ Easy/05-count-items-matching-rule/countMatches.js
@@ -0,0 +1,12 @@
+//link: https://leetcode.com/problems/count-items-matching-a-rule/
+
+function countMatches(items, ruleKey, ruleValue) {
+  //initialize an accumulator
+  //iterate through items
+  //for each item that matches the ruleKey with the ruleValue value add 1 to the accumulator
+  //return accumulator
+}
+
+module.exports = {
+    countMatches
+};
---
+++ Easy/05-count-items-matching-rule/countMatches.test.js
@@ -0,0 +1,15 @@
+const { countMatches } = require('./countMatches');
+
+describe('countMatches fxn', () => {
+    it('takes in 3 parameters: an array of string arrays that each have a type, color, and name (in that order), and two string parameters, ruleValue and ruleKey. It returns the amount of objects in the array that match up with the ruleKey and ruleValue strings.', () => {
+        const expected1 = 1;
+        const actual1 = countMatches([['phone', 'blue', 'pixel'], ['computer', 'silver', 'lenovo'], ['phone', 'gold', 'iphone']], 'color', 'silver');
+
+        expect(expected1).toEqual(actual1);
+
+        const expected2 = 2;
+        const actual2 = countMatches([['phone', 'blue', 'pixel'], ['computer', 'silver', 'phone'], ['phone', 'gold', 'iphone']], 'type', 'phone');
+
+        expect(expected2).toEqual(actual2);
+    });
+});
