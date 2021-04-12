2b1717a3e48914941db8c19493a47c2a4b484e89
Shane Upchurch <shaneupchurch@gmail.com>
2021-04-11 20:17:58 -0500

    solves goalParser and passes test

--- Easy/03-jewels-and-stones/numJewelsInStones.js
+++ Easy/03-jewels-and-stones/numJewelsInStones.js
@@ -2,21 +2,21 @@
 
 function numJewelsInStones(jewels, stones) {
   //initialize an empty "answer" array
-  let answer = 0;
+    let answer = 0;
   //compare the first letter in jewels to each letter in stones and add 1 to the answer array for each match
   //**constraints say that jewels will always be <= 1 and that all characters of jewels are unique**
-  for (let i = 0; i <= jewels.length; i++) {
-    for (let j of stones) {
-      if (jewels[i] === j) {
-        answer++;
-      }
+    for (let i = 0; i <= jewels.length; i++) {
+        for (let j of stones) {
+            if (jewels[i] === j) {
+                answer++;
+            }
+        }
     }
-  }
   //repeat process until you've checked each letter in jewels and return the answer array
-  return answer;
+    return answer;
   //bonus points for regex!
 }
 
 module.exports = {
-  numJewelsInStones
+    numJewelsInStones
 };
--- Easy/04-goal-parser/goalParser.js
+++ Easy/04-goal-parser/goalParser.js
@@ -2,18 +2,43 @@
 
 function goalParser(command) {
   // take the incoming string and split it up, looking for patters of either 'g', '()', or '(al)'. 
-
+    const commandArr = [...command]; //this isn't what you meant, I don't believe, so I'd be interested to hear what you had in mind
   // initialize new array 
+    let answerString = '';
 
   // g can be pushed into the array as is.
-  // () should instead push an 'o' into the array. and 'al' instead of '(al)'. 
-
+  // () should instead push an 'o' into the array. and 'al' instead of '(al)'.
   // iterate through the array and join each string into one new string.
-
-  // return string
-
+    for (let i = 0; i < commandArr.length; i++) {
+        if (commandArr[i] === 'G') {
+            answerString = answerString.concat('G');
+        } 
+        else if (commandArr[i] === '(') {
+            if (commandArr[i + 1] === ')') {
+                answerString = answerString.concat('o');
+                i = i + 1;
+            } 
+            if (commandArr[i + 1] === 'a') {
+                answerString = answerString.concat('al');
+                i = i + 3;
+            }
+        // if (commandArr[i] === '(' && commandArr[i + 1] === ')') {
+        //     answerString = answerString.concat('o');
+        //     i = i + 1;
+        //     console.log('inside if', i);
+        // }
+        // else if (
+        //     commandArr[i] === '(' && commandArr[i + 1] === 'a') {
+        //     console.log(commandArr[i + 1]);
+        //     answerString.concat('al');
+        //     i = i + 3;
+        // }
+        }
+    }
+    // return string
+    return answerString;
 }
 
 module.exports = {
-  goalParser
+    goalParser
 };
