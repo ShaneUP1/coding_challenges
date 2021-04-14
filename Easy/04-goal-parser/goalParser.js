e47e9470f6a0f2cbe2589f413c07bd7e3ee2f4c9
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-14 09:35:02 -0400

    passing countMatches

--- Easy/04-goal-parser/goalParser.js
+++ Easy/04-goal-parser/goalParser.js
@@ -2,43 +2,44 @@
 
 function goalParser(command) {
   // take the incoming string and split it up, looking for patters of either 'g', '()', or '(al)'. 
-    const commandArr = [...command]; //this isn't what you meant, I don't believe, so I'd be interested to hear what you had in mind
+  const commandArr = [...command]; //this isn't what you meant, I don't believe, so I'd be interested to hear what you had in mind
+
   // initialize new array 
-    let answerString = '';
+  let answerString = '';
 
   // g can be pushed into the array as is.
   // () should instead push an 'o' into the array. and 'al' instead of '(al)'.
   // iterate through the array and join each string into one new string.
-    for (let i = 0; i < commandArr.length; i++) {
-        if (commandArr[i] === 'G') {
-            answerString = answerString.concat('G');
-        } 
-        else if (commandArr[i] === '(') {
-            if (commandArr[i + 1] === ')') {
-                answerString = answerString.concat('o');
-                i = i + 1;
-            } 
-            if (commandArr[i + 1] === 'a') {
-                answerString = answerString.concat('al');
-                i = i + 3;
-            }
-        // if (commandArr[i] === '(' && commandArr[i + 1] === ')') {
-        //     answerString = answerString.concat('o');
-        //     i = i + 1;
-        //     console.log('inside if', i);
-        // }
-        // else if (
-        //     commandArr[i] === '(' && commandArr[i + 1] === 'a') {
-        //     console.log(commandArr[i + 1]);
-        //     answerString.concat('al');
-        //     i = i + 3;
-        // }
-        }
+  for (let i = 0; i < commandArr.length; i++) {
+    if (commandArr[i] === 'G') {
+      answerString = answerString.concat('G');
     }
-    // return string
-    return answerString;
+    else if (commandArr[i] === '(') {
+      if (commandArr[i + 1] === ')') {
+        answerString = answerString.concat('o');
+        i = i + 1;
+      }
+      if (commandArr[i + 1] === 'a') {
+        answerString = answerString.concat('al');
+        i = i + 3;
+      }
+      // if (commandArr[i] === '(' && commandArr[i + 1] === ')') {
+      //     answerString = answerString.concat('o');
+      //     i = i + 1;
+      //     console.log('inside if', i);
+      // }
+      // else if (
+      //     commandArr[i] === '(' && commandArr[i + 1] === 'a') {
+      //     console.log(commandArr[i + 1]);
+      //     answerString.concat('al');
+      //     i = i + 3;
+      // }
+    }
+  }
+  // return string
+  return answerString;
 }
 
 module.exports = {
-    goalParser
+  goalParser
 };
--- Easy/05-count-items-matching-rule/countMatches.test.js
+++ Easy/05-count-items-matching-rule/countMatches.test.js
@@ -2,6 +2,7 @@ const { countMatches } = require('./countMatches');
 
 describe('countMatches fxn', () => {
     it('takes in 3 parameters: an array of string arrays that each have a type, color, and name (in that order), and two string parameters, ruleValue and ruleKey. It returns the amount of objects in the array that match up with the ruleKey and ruleValue strings.', () => {
+
         const expected1 = 1;
         const actual1 = countMatches([['phone', 'blue', 'pixel'], ['computer', 'silver', 'lenovo'], ['phone', 'gold', 'iphone']], 'color', 'silver');
 
