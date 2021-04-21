4a960769e81d4acce20c02e9e5612dddf4447a46
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-20 20:58:07 -0400

    saving work

--- Easy/07-nearest-point/nearestValidPoint.js
+++ Easy/07-nearest-point/nearestValidPoint.js
@@ -7,9 +7,34 @@
 // The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
 
 function nearestValidPoint(x, y, points) {
-  //iterate through the points arrays
-  //1. for each subarray of points verify that it shares either the same x or y coordinate
-  //2. compute the Manhattan distance of the points
+  let index = -1;
+  let minimum = Infinity;
+  // iterate through the points arrays
+  for (let i = 0; i < points.length; i++) {
+    let point = points[i];
+    //1. for each subarray of points verify that it shares either the same x or y coordinate
+    if (point[0] === x || point[1] === y) {
+      //2. compute the Manhattan distance of the points
+      let manhattan = Math.abs(x - point[0]) + Math.abs(y - point[1]);
+      if (manhattan < minimum) {
+        minimum = manhattan;
+        index = i;
+      }
+    }
+  }
+  return minimum;
+
+  // points.forEach(([a, b], i) => {
+  //   if (a === x || b === y) {
+  //     const distance = Math.abs(x - a) + Math.abs(y - b)
+  //     if (distance < min) {
+  //       idx = i
+  //       min = distance
+  //     }
+  //   }
+  // })
+  // return idx;
+
   //3. if the distance is the smallest found, record it and the index (if it's a tie for shortest distance, keep original and toss out latest)
   //repeat steps 1-3 for each of the subarrays
   //return the index with the smallest distance
@@ -17,5 +42,5 @@ function nearestValidPoint(x, y, points) {
 }
 
 module.exports = {
-    nearestValidPoint
+  nearestValidPoint
 };
--- Easy/07-nearest-point/nearestValidPoint.test.js
+++ Easy/07-nearest-point/nearestValidPoint.test.js
@@ -1,19 +1,20 @@
+const { nearestValidPoint } = require('./nearestValidPoint');
 
 describe('nearestValidPoint fxn', () => {
     it('takes in 2 coordinate point, x and y, and an array of points and it returns the index of the points with the smallest Manhattan Distance from the x and y coordinates', () => {
-      
+
         const expected1 = 2;
         const actual1 = nearestValidPoint(3, 4, [[1, 2], [3, 1], [2, 4], [2, 3], [4, 4]]);
 
         expect(actual1).toEqual(expected1);
 
         const expected2 = 0;
-        const actual2 = nearestValidPoint(3, 4, [[3,4]]);
+        const actual2 = nearestValidPoint(3, 4, [[3, 4]]);
 
         expect(actual2).toEqual(expected2);
 
         const expected3 = -1;
-        const actual3 = nearestValidPoint(3, 4, [[2,3]]);
+        const actual3 = nearestValidPoint(3, 4, [[2, 3]]);
 
         expect(actual3).toEqual(expected3);
     });
